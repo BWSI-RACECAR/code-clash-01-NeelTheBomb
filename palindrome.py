5c988e65763091979f2c04ebb37305417b42be9a
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-07-12 09:05:02 -0400

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    # Write code below to complete prompt
+    def isPalindrome(self, s):
+            #type s: string
+            #return type: boolean
+            if (len(s) < 6):
+                 return False
+            g = s.reverse
+            if (g == s):
+                return True
+            
+            
+            #TODO: Write code below to return a boolean value with the solution to the prompt.
+            pass
+
+def main():
+    tc1 = Solution()
+    inpyt = input()
+    # Write code below to complete prompt
+    print(tc1.isPalindrome(inpyt))
+
+if __name__ == "__main__":
+    main()
